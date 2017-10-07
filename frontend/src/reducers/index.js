d2a9dce71a2694fcde2092904a515d915806cb1f
Botmasher <languagejosh@hotmail.com>
2017-10-07 12:19:20 -0700

    feat: Scaffold out redux store sketch from specifications

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,44 @@
+import { combineReducers } from 'redux';
+
+const initialPostState = {
+	id: null, 				// unique string identifier
+	timestamp: null, 		// default Unix time track data
+	title: null, 			// string
+	body: null, 			// string
+	author: null, 			// string
+	category: [], 			// should be one of the categories provided by server
+	voteScore: 1, 			// net votes post has received (default: 1)
+	deleted: false 			// flag not to show post in front end
+};
+
+const initialCommentState = {
+	id: null, 				// unique string identifier
+	parentId: null, 		// dependency
+	timestamp: null, 		// default Unix time track data
+	body: null, 			// string
+	author: null, 			// string
+	category: [], 			// should be one of the categories provided by server
+	voteScore: 1, 			// net votes post has received (default: 1)
+	deleted: false 			// flag not to show post in front end
+	parentDeleted: false	// parent can be deleted but this comment not
+};
+
+function post(state=initialPostState, action) {
+	switch (action.type) {
+		case ADD_POST:
+			return { ... state };
+		default:
+			return state;
+	}
+}
+
+function comment(state=initialCommentState, action) {
+	switch (action.type) {
+		case ADD_COMMENT:
+			return { ...state };
+		default:
+			return state;
+	}
+}
+
+export default combineReducers({ post, comment });
