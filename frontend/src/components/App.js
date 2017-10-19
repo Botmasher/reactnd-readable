6f5ea224e2b1f3ac70c941853c22fdb1ea99c104
Botmasher <languagejosh@hotmail.com>
2017-10-18 20:48:04 -0600

    refactor: Remove test routing from main app component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,15 +3,11 @@ import TestAPI from './TestAPI';
 import { Route } from 'react-router-dom';
 
 function App(props) {
-
-	const strange_array = ['asdf', '123', 'gwarg', 'something_else', 'wibbidietsagguiddy', 'long_with_variables?q=happy&l=waaaah'];
-	
 	return (
 		<div>
-			<TestAPI all={false} posts={false} comments={false} />
-			<Route render={({history}) => (
-				<button type="button" onClick={() => { history.push(`/${strange_array[Math.floor(Math.random()*(strange_array.length))]}`) }}>Go sumwherz</button>
-			)} />
+			<Route render={() => {
+				<TestAPI all={false} posts={false} comments={false} />
+			}}/>
 		</div>
 	);
 }
